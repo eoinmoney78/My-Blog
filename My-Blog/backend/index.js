07d1e3c3a66a16540cfbcdbd5be2966a84638034
eoinmoney78 <94717367+eoinmoney78@users.noreply.github.com>
2023-08-08 15:14:40 -0400

    commit

--- My-Blog/backend/index.js
+++ My-Blog/backend/index.js
@@ -8,9 +8,12 @@ const postRoute = require("./routes/posts");
 const categoryRoute = require("./routes/categories");
 const multer = require("multer");
 const path = require("path");
+
+// const cors = require('cors');
 dotenv.config();
 
 app.use(express.json());
+// app.use(cors());
 app.use("/images", express.static(path.join(__dirname, "/images")));
 mongoose.connect(process.env.MONGO_URL, {
 
@@ -18,19 +21,21 @@ mongoose.connect(process.env.MONGO_URL, {
 
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
-        cb(null, req.body.name);
+        cb(null, "images"); // set the destination to the images folder
     },
     filename: (req, file, cb) => {
-        cb(null, req.body.name);
+        cb(null, Date.now() + path.extname(file.originalname)); // generate a unique filename using the current timestamp
     },
 });
-
 
 const upload = multer({ storage: storage });
 
 
+
 app.post("/upload", upload.single("file"), (req, res) => {
     res.status(200).json("File has been uploaded");
+}, (error, req, res, next) => { // Error handling middleware
+    res.status(400).json({ error: error.message });
 });
 
 app.use("/auth", authRoute);
--- My-Blog/backend/routes/auth.js
+++ My-Blog/backend/routes/auth.js
@@ -1,3 +1,43 @@
+// const router = require("express").Router();
+// const User = require("../models/User");
+// const bcrypt = require("bcrypt");
+
+// //REGISTER
+// router.post("/register", async (req, res) => {
+//     try {
+//         const salt = await bcrypt.genSalt(10);
+//         const hashedPass = await bcrypt.hash(req.body.password, salt);
+//         const newUser = new User({
+//             username: req.body.username,
+//             email: req.body.email,
+//             password: hashedPass,
+//         });
+
+//         const user = await newUser.save();
+//         res.status(200).json(user);
+//     } catch (err) {
+//         res.status(500).json(err);
+//     }
+// });
+
+// router.post("/login", async (req, res) => {
+//     try {
+//         const user = await User.findOne({ username: req.body.username });
+//         if (!user) return res.status(400).json("Wrong credentials!");
+
+//         const validated = await bcrypt.compare(req.body.password, user.password);
+//         if (!validated) return res.status(400).json("Wrong credentials!");
+
+//         const { password, ...others } = user._doc;
+//         res.status(200).json(others);
+//     } catch (err) {
+//         res.status(500).json(err);
+//     }
+// });
+
+
+// module.exports = router;
+
 const router = require("express").Router();
 const User = require("../models/User");
 const bcrypt = require("bcrypt");
@@ -5,6 +45,11 @@ const bcrypt = require("bcrypt");
 //REGISTER
 router.post("/register", async (req, res) => {
     try {
+        // Validate request data (simple example - consider a library like `joi` for more comprehensive validation)
+        if (!req.body.username || !req.body.password || !req.body.email) {
+            return res.status(400).json({ message: "Required field missing" });
+        }
+
         const salt = await bcrypt.genSalt(10);
         const hashedPass = await bcrypt.hash(req.body.password, salt);
         const newUser = new User({
@@ -14,25 +59,34 @@ router.post("/register", async (req, res) => {
         });
 
         const user = await newUser.save();
-        res.status(200).json(user);
+
+        // Return without password
+        const { password, ...others } = user._doc;
+        res.status(201).json(others); // 201 Created is more appropriate for a successful registration
     } catch (err) {
-        res.status(500).json(err);
+        res.status(500).json({ message: "Error registering user" });
     }
 });
 
-//LOGIN
 router.post("/login", async (req, res) => {
     try {
+        // Validate request data
+        if (!req.body.username || !req.body.password) {
+            return res.status(400).json({ message: "Required field missing" });
+        }
+
         const user = await User.findOne({ username: req.body.username });
-        !user && res.status(400).json("Wrong credentials!");
+
+        // Using the same message for both cases to not reveal specifics
+        if (!user) return res.status(400).json({ message: "Invalid credentials" });
 
         const validated = await bcrypt.compare(req.body.password, user.password);
-        !validated && res.status(400).json("Wrong credentials!");
+        if (!validated) return res.status(400).json({ message: "Invalid credentials" });
 
         const { password, ...others } = user._doc;
         res.status(200).json(others);
     } catch (err) {
-        res.status(500).json(err);
+        res.status(500).json({ message: "Error logging in" });
     }
 });
 
